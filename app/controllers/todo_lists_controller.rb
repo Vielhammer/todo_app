4a21cd4ea2ca049cf66136712132aaea87ec406c
Vielhammer <75920255+Vielhammer@users.noreply.github.com>
2021-09-14 18:33:41 -0500

    TodoList
    - Implemented TodoList model
    - Added validation
    - Added basic index page and styling

---
+++ app/controllers/todo_lists_controller.rb
@@ -0,0 +1,21 @@
+class TodoListsController < ApplicationController
+	def index
+		@todo_lists = TodoList.all
+		@todo_list = TodoList.new
+	end
+
+	def create
+		@todo_list = TodoList.new(todo_list_params)
+
+		if @todo_list.save
+			redirect_to todo_lists_url, notice: 'Todo List has been successfully created.'
+		else
+			# render :new 
+		end 
+	end
+
+	private
+		def todo_list_params
+			params.require(:todo_list).permit(:title)
+		end
+end
