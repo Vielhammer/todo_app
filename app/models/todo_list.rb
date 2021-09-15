386f603286a27e19d07dbdbdddaafebcd58fe6b8
Vielhammer <75920255+Vielhammer@users.noreply.github.com>
2021-09-14 19:41:37 -0500

    Refactor
    - Implemented Stimulus-Reflex
    - Refactored views
    - Added reflex for todo_items

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -2,4 +2,17 @@ class TodoList < ApplicationRecord
 	has_many :todo_items, dependent: :destroy
 	
 	validates :title, presence: true
+
+	def percent_completed
+    return 0 if total_items == 0
+    ( 100 * completed_items.to_f / total_items).round(1)  
+  end
+
+  def completed_items 
+    @completed_items ||= todo_items.completed.count
+  end
+
+  def total_items
+    @total_items ||= todo_items.count
+  end
 end
